62a3bd017cf740c8023681ff6e7d018291d965ad
Zachary Humphrey <70534409+zacharyjhumphrey@users.noreply.github.com>
2022-03-05 12:17:13 -0600

    tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -22,6 +22,21 @@ def test_merge_sort():
     assert sorted(arr) == merge_sort(arr)
 
 
+def test_create_sorted_array():
+    arr = create_sorted_array()
+    assert sorted(arr) == arr
+    failing_arr = [3, 4, 5, 12, 9]
+    assert sorted(failing_arr) != failing_arr
+
+
 def test_create_reversed_array():
     arr = create_reversed_array()
     assert sorted(arr) == list(reversed(arr))
+    failing_arr = [3, 4, 8, 5]
+    assert sorted(failing_arr) != list(reversed(failing_arr))
+
+
+def test_create_array_of_one_value():
+    arr = create_array_of_one_value()
+    single_value = arr[0]
+    assert all(map(lambda x: x == single_value, arr))
